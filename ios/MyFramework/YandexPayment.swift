f11779d6f397ba559a3a3217becb10039c49c6d2
YuriFA <yurik45gb@gmail.com>
2021-09-16 16:18:22 +0300

    add applePayMerchantIdentifier param to TokenizationModuleInputData

--- ios/MyFramework/YandexPayment.swift
+++ ios/MyFramework/YandexPayment.swift
@@ -51,6 +51,7 @@ class YandexPayment: RCTViewManager, TokenizationModuleOutput {
             name: map["SHOP_NAME"] as! String,
             description: map["SHOP_DESCRIPTION"] as! String,
             returnUrl: map["SHOP_RETURN_URL"] as! String,
+            applePayMerchantIdentifier: map["SHOP_APPLEPAY_MERCHANT_IDENTIFIER"] as! String,
             applicationScheme: map["SHOP_APPLICATION_SCHEME"] as! String
         )
         
@@ -70,6 +71,7 @@ class YandexPayment: RCTViewManager, TokenizationModuleOutput {
             tokenizationSettings: TokenizationSettings(paymentMethodTypes: PaymentMethodTypes(rawValue: payment.types)),
             savePaymentMethod: payment.savePaymentMethod,
             moneyAuthClientId: payment.moneyAuthClientId,
+            applePayMerchantIdentifier: shop.applePayMerchantIdentifier,
             applicationScheme: shop.applicationScheme
         )
         let inputData: TokenizationFlow = .tokenization(moduleInputData)
